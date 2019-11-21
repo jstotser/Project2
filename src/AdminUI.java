f4728efc33ea9ad777d38508f62c1c9841a9c1a1
jstotser <47337700+jstotser@users.noreply.github.com>
2019-11-21 00:37:15 -0600

    Add files via upload

---
+++ src/AdminUI.java
@@ -0,0 +1,58 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AdminUI {
+    public JFrame view;
+
+    public JButton btnSystemConfiguration = new JButton("Setup System Configuration");
+    public JButton btnAddNewUser= new JButton("Add New User");
+    public JButton btnDeleteUser = new JButton("Delete Existing User");
+
+    public AdminUI(UserModel user) {
+        this.view = new JFrame();
+
+        view.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        view.setTitle("Store Management System - Cashier View");
+        view.setSize(1000, 600);
+        view.getContentPane().setLayout(new BoxLayout(view.getContentPane(), BoxLayout.PAGE_AXIS));
+
+        JLabel title = new JLabel("Store Management System");
+
+        title.setFont (title.getFont ().deriveFont (24.0f));
+        view.getContentPane().add(title);
+
+        JPanel panelButtons = new JPanel(new FlowLayout());
+        panelButtons.add(btnSystemConfiguration);
+        panelButtons.add(btnAddNewUser);
+        panelButtons.add(btnDeleteUser);
+
+        view.getContentPane().add(panelButtons);
+
+
+        btnSystemConfiguration.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                AddPurchaseUI ap = new AddPurchaseUI();
+                ap.run();
+            }
+        });
+
+        btnAddNewUser.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                AddPurchaseUI ap = new AddPurchaseUI();
+                ap.run();
+            }
+        });
+
+        btnDeleteUser.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                AddPurchaseUI ap = new AddPurchaseUI();
+                ap.run();
+            }
+        });
+    }
+}
